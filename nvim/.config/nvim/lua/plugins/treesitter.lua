28c60f0980643497420882aec4e039af653fca08
Jaime Luis Mesa Viquez <jaimemesaviquez@gmail.com>
2026-01-16 19:39:27 +0100

    Nvim treesitter fixed

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,27 +1,28 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  config = function()
-    require('nvim-treesitter.configs').setup({
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
       ensure_installed = {
-        'lua',
-        'vim',
-        'vimdoc',
-        'markdown',
-        'gdscript',
-        'godot_resource',
-        'gdshader',
-        'cpp',
-        'c',
+        "lua",
+        "vim",
+        "vimdoc",
+        "markdown",
+        "gdscript",
+        "godot_resource",
+        "gdshader",
+        "cpp",
+        "c",
       },
       sync_install = false,
       auto_install = true,
       highlight = {
         enable = true,
+        additional_vim_regex_highlighting = false,
       },
       indent = {
         enable = true,
       },
-    })
-  end,
+    },
+  },
 }
